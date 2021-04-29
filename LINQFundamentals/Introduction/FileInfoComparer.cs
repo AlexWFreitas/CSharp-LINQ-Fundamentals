bdc5e3d9ebd4eb1f6b3ed87802a62800efc60194
Alex W. de Freitas <otaru1921@gmail.com>
2021-04-29 16:09:05 -0300

    Chapter 3 - 20%.

---
+++ LINQFundamentals/Introduction/FileInfoComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Introduction
+{
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        // Compare compares two files and then orders then by smaller to higher ( Ascending ) -1 if lower, 0 if equal, 1 if higher
+        // Our code inside compares and returns -1 if bigger, 0 if equal, 1 if lower.
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
