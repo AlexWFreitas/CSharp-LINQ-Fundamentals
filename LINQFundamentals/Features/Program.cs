4c5976aae5dd0002dc4c84fe0c1ef6d6f17bc8d9
Alex W. de Freitas <otaru1921@gmail.com>
2021-05-03 18:45:36 -0300

    Chapter 3 - Done
    Chapter 4 - Done
    Chapter 5 - Started

--- LINQFundamentals/Features/Program.cs
+++ LINQFundamentals/Features/Program.cs
@@ -29,7 +29,7 @@ namespace Features
                 new Employee { Id = 2, Name = "Chris" }
             };
 
-            IEnumerable<Employee> sales = new List<Employee>()
+            var sales = new List<Employee>()
             {
                 new Employee { Id = 3, Name = "Alex" }
             };
@@ -45,8 +45,14 @@ namespace Features
 
             Console.WriteLine("\nDevelopers ordered alphabetically only with names with exactly 5 letters:");
 
-            var query = developers.Where(e => e.Name.Length == 5).
-                                                OrderBy(e => e.Name);
+            var query = developers.Where(e => e.Name.Length == 5)
+                                  .OrderBy(e => e.Name);
+
+            var query2 =
+                from developer in developers
+                where developer.Name.Length == 5
+                orderby developer.Name
+                select developer;
 
             foreach (var employee in query)
                 Console.WriteLine(employee.Name);
---
+++ LINQFundamentals/Queries/MyLinq.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queries
+{
+    public static class MyLinq
+    {
+        /// <summary>
+        /// Deferred Execution - Where Clone
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Predicate<T> predicate)
+        {
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                    yield return item;
+            }   
+        }
+
+        /// <summary>
+        /// Where Clone that has No Deferred Execution
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> OtherFilter<T>(this IEnumerable<T> source, Predicate<T> predicate)
+        {
+            var sequence = new List<T>();
+            foreach (var item in source)
+            {
+                if (predicate(item))
+                    sequence.Add(item);
+            }
+            return sequence;
+        }
+
+        public static IEnumerable<double> Random()
+        {
+            var random = new Random();
+            while(true)
+            {
+                yield return random.NextDouble();
+            }
+        }
+    }
+}
