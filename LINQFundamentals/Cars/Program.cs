256d99567bc141a42667a9c233cd43f3e3857842
Alex W. de Freitas <otaru1921@gmail.com>
2021-05-04 11:16:18 -0300

    Chapter 5 - 70%

--- LINQFundamentals/Cars/Program.cs
+++ LINQFundamentals/Cars/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Cars
 {
@@ -6,7 +9,25 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cars = ProcesssFile("fuel.csv");
+
+            var query = cars.OrderByDescending(car => car.Combined)
+                            .ThenBy(car => car.Name);
+
+            foreach (var car in query.Take(10))
+            {
+                Console.WriteLine($"{car.Name} : {car.Combined}");
+            }
+        }
+
+        private static List<Car> ProcesssFile(string path)
+        {
+            return
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(line => line.Length > 1)
+                    .Select(Car.ParseFromCsv)
+                    .ToList();
         }
     }
 }
